26b1126ee712ed6a1615f30e6d4281b3cdb9b3cc
Michael Dawson <mdawson@devrus.com>
2023-06-02 16:28:04 -0400

    fixup after testing in local repo
    Signed-off-by: Michael Dawson <mdawson@devrus.com>

--- scripts/create.sh
+++ scripts/create.sh
@@ -6,7 +6,7 @@ set -o pipefail
 readonly PROG_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly ROOT_DIR="$(cd "${PROG_DIR}/.." && pwd)"
 
-bash $PROG_DIR/create.sh
-STACK_DIR=${ROOT_DIR}/stack-nodejs-16 BUILD_DIR=${ROOT_DIR}/build-nodejs-16 bash $PROG_DIR/create.sh
-STACK_DIR=${ROOT_DIR}/stack-nodejs-18 BUILD_DIR=${ROOT_DIR}/build-nodejs-18 bash $PROG_DIR/create.sh
+bash $PROG_DIR/create-one.sh
+STACK_DIR=${ROOT_DIR}/stack-nodejs-16 BUILD_DIR=${ROOT_DIR}/build-nodejs-16 bash $PROG_DIR/create-one.sh
+STACK_DIR=${ROOT_DIR}/stack-nodejs-18 BUILD_DIR=${ROOT_DIR}/build-nodejs-18 bash $PROG_DIR/create-one.sh
 
