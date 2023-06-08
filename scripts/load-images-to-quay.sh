c2798e683358e03bcab56a028a8f0ee13027138c
Michael Dawson <mdawson@devrus.com>
2023-06-08 18:23:17 -0400

    remove uneeded ENVs in docker files for stack
    Signed-off-by: Michael Dawson <mdawson@devrus.com>

--- scripts/load-images-to-quay.sh
+++ scripts/load-images-to-quay.sh
@@ -6,7 +6,7 @@ readonly PROG_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 bash $PROG_DIR/create.sh
 
 skopeo copy --dest-tls-verify=false oci-archive:../build/run.oci docker-daemon:quay.io/midawson/ubi8-paketo-run:latest
-skopeo copy --dest-tls-verify=false oci-archive:../build/run.oci docker-daemon:quay.io/midawson/ubi8-paketo-build:latest
+skopeo copy --dest-tls-verify=false oci-archive:../build/build.oci docker-daemon:quay.io/midawson/ubi8-paketo-build:latest
 skopeo copy --dest-tls-verify=false oci-archive:../build-nodejs-16/run.oci docker-daemon:quay.io/midawson/ubi8-paketo-run-nodejs-16:latest
 skopeo copy --dest-tls-verify=false oci-archive:../build-nodejs-18/run.oci docker-daemon:quay.io/midawson/ubi8-paketo-run-nodejs-18:latest
 
