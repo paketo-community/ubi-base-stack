85c2628936c85ea13d404b4aee6f54c963110905
Ozzy Osborne <bardweller@gmail.com>
2023-10-24 16:18:48 -0400

    Add Java run images
    Signed-off-by: Ozzy Osborne <bardweller@gmail.com>

--- scripts/create.sh
+++ scripts/create.sh
@@ -9,4 +9,7 @@ readonly ROOT_DIR="$(cd "${PROG_DIR}/.." && pwd)"
 bash $PROG_DIR/create-one.sh
 STACK_DIR=${ROOT_DIR}/stack-nodejs-16 BUILD_DIR=${ROOT_DIR}/build-nodejs-16 bash $PROG_DIR/create-one.sh
 STACK_DIR=${ROOT_DIR}/stack-nodejs-18 BUILD_DIR=${ROOT_DIR}/build-nodejs-18 bash $PROG_DIR/create-one.sh
+STACK_DIR=${ROOT_DIR}/stack-java-8 BUILD_DIR=${ROOT_DIR}/build-java-8 bash $PROG_DIR/create-one.sh
+STACK_DIR=${ROOT_DIR}/stack-java-11 BUILD_DIR=${ROOT_DIR}/build-java-11 bash $PROG_DIR/create-one.sh
+STACK_DIR=${ROOT_DIR}/stack-java-17 BUILD_DIR=${ROOT_DIR}/build-java-17 bash $PROG_DIR/create-one.sh
 
---
+++ stack-java-17/run.Dockerfile
@@ -0,0 +1,1 @@
+FROM registry.access.redhat.com/ubi8/openjdk-17-runtime
